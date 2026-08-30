dfc78d27fc0f422c30a056c98525f9e759eb4c14
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-08-30 13:39:07 +0200

    Adjust soundcloud embeds to be grids

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,80 +20,94 @@ export default function Home() {
 					words={["some of the music i made:"]}
 					textClassName="text-2xl"
 				/>
-				<div>
-					<iframe
-						width="100%"
-						height="300"
-						allow="autoplay; encrypted-media"
-						src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2361209465&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-						className="rounded-lg"
-					/>
-					<div
-						style={{
-							fontSize: "10px",
-							color: "#cccccc",
-							lineBreak: "anywhere",
-							overflow: "hidden",
-							fontFamily:
-								"Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
-							fontWeight: 100,
-						}}
-					>
-						<a
-							href="https://soundcloud.com/xvcf"
-							title="xvcf"
-							target="_blank"
-							style={{ color: "#cccccc", textDecoration: "none" }}
+				<div className="grid grid-cols-2 grid-rows-1 gap-4">
+					<div>
+						<iframe
+							width="100%"
+							height="300"
+							allow="autoplay; encrypted-media"
+							src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2361209465&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+							className="rounded-lg"
+						/>
+						<div
+							style={{
+								fontSize: "10px",
+								color: "#cccccc",
+								lineBreak: "anywhere",
+								overflow: "hidden",
+								fontFamily:
+									"Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
+								fontWeight: 100,
+							}}
 						>
-							xvcf
-						</a>{" "}
-						·{" "}
-						<a
-							href="https://soundcloud.com/xvcf/super-taxi-sim-theme/"
-							title="Super Taxi Sim - Theme Song"
-							target="_blank"
-							style={{ color: "#cccccc", textDecoration: "none" }}
-						>
-							Super Taxi Sim - Main Theme
-						</a>
+							<a
+								href="https://soundcloud.com/xvcf"
+								title="xvcf"
+								target="_blank"
+								style={{
+									color: "#cccccc",
+									textDecoration: "none",
+								}}
+							>
+								xvcf
+							</a>{" "}
+							·{" "}
+							<a
+								href="https://soundcloud.com/xvcf/super-taxi-sim-theme/"
+								title="Super Taxi Sim - Theme Song"
+								target="_blank"
+								style={{
+									color: "#cccccc",
+									textDecoration: "none",
+								}}
+							>
+								Super Taxi Sim - Main Theme
+							</a>
+						</div>
 					</div>
-				</div>
-				<div>
-					<iframe
-						width="100%"
-						height="300"
-						allow="autoplay; encrypted-media"
-						src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2361211655&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-						className="rounded-lg"
-					/>
-					<div
-						style={{
-							fontSize: "10px",
-							color: "#cccccc",
-							lineBreak: "anywhere",
-							overflow: "hidden",
-							fontFamily:
-								"Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
-							fontWeight: 100,
-						}}
-					>
-						<a
-							href="https://soundcloud.com/xvcf"
-							title="xvcf"
-							target="_blank"
-							style={{ color: "#cccccc", textDecoration: "none" }}
+					<div>
+						<iframe
+							width="100%"
+							height="300"
+							allow="autoplay; encrypted-media"
+							src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2361211655&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+							className="rounded-lg"
+						/>
+						<div
+							style={{
+								fontSize: "10px",
+								color: "#cccccc",
+								lineBreak: "anywhere",
+								overflow: "hidden",
+								fontFamily:
+									"Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif",
+								fontWeight: 100,
+							}}
 						>
-							xvcf
-						</a>{" "}
-						·{" "}
-						<a
-							href="https://soundcloud.com/xvcf/synthwave-alt"
-							title="Synthwave Alt"
-							target="_blank"
-							style={{ color: "#cccccc", textDecoration: "none" }}
-						>
-							Synthwave Alt
-						</a>
+							<a
+								href="https://soundcloud.com/xvcf"
+								title="xvcf"
+								target="_blank"
+								style={{
+									color: "#cccccc",
+									textDecoration: "none",
+								}}
+							>
+								xvcf
+							</a>{" "}
+							·{" "}
+							<a
+								href="https://soundcloud.com/xvcf/synthwave-alt"
+								title="Synthwave Alt"
+								target="_blank"
+								style={{
+									color: "#cccccc",
+									textDecoration: "none",
+								}}
+							>
+								Synthwave Alt
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>
